1e4a0aed09440e49dce76c822bda3aca9bd577c0
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2020-09-12 12:00:49 +0000

    Automated deployment: Sat Sep 12 12:00:49 UTC 2020 201c9e6181df1f6e240547d2d9a6e60f134bbedd

--- libportal/desktop/account/sidebar-items.js
+++ libportal/desktop/account/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["AccountProxy","The interface lets sandboxed applications query basic information about the user, like his name and avatar photo."],["UserInfo",""],["UserInfoOptions","Specified the options for a get user information request."],["UserInfoResponse",""]]});
+initSidebarItems({"struct":[["AccountProxy","The interface lets sandboxed applications query basic information about the user, like his name and avatar photo."],["UserInfo",""],["UserInfoOptions","Specified the options for a get user information request."]]});
--- libportal/desktop/wallpaper/sidebar-items.js
+++ libportal/desktop/wallpaper/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["SetOn","Where to set the wallpaper on."]],"struct":[["WallpaperOptions","Specified options for a set wallpaper request."],["WallpaperProxy","The interface lets sandboxed applications set the user's desktop background picture."],["WallpaperResponse",""]]});
+initSidebarItems({"enum":[["SetOn","Where to set the wallpaper on."]],"struct":[["WallpaperOptions","Specified options for a set wallpaper request."],["WallpaperProxy","The interface lets sandboxed applications set the user's desktop background picture."]]});
