8a222a3a91e5baf5e90828a2d7c848ba60243e12
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2020-09-13 02:21:27 +0000

    Automated deployment: Sun Sep 13 02:21:27 UTC 2020 ed500a9c1ca775664a504ccf8933d9d9aff83b62

--- libportal/flatpak/update_monitor/sidebar-items.js
+++ libportal/flatpak/update_monitor/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["UpdateMonitorProxy","The interface exposes some interactions with Flatpak on the host to the sandbox. For example, it allows you to restart the applications or start a more sandboxed instance."],["UpdateOptions",""]]});
+initSidebarItems({"enum":[["UpdateStatus",""]],"struct":[["UpdateInfo",""],["UpdateMonitorProxy","The interface exposes some interactions with Flatpak on the host to the sandbox. For example, it allows you to restart the applications or start a more sandboxed instance."],["UpdateOptions",""],["UpdateProgress",""]]});
