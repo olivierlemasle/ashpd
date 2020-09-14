89d401f134fcf1547f62b93b630bfcb8ecf4897f
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2020-09-14 20:44:57 +0000

    Automated deployment: Mon Sep 14 20:44:57 UTC 2020 c2dbe627753dc614ab292bbbb60ba4f9a8611a33

--- ashpd/desktop/account/sidebar-items.js
+++ ashpd/desktop/account/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["AccountProxy","The interface lets sandboxed applications query basic information about the user, like his name and avatar photo."],["UserInfo",""],["UserInfoOptions","Specified the options for a get user information request."]]});
+initSidebarItems({"struct":[["AccountProxy","The interface lets sandboxed applications query basic information about the user, like his name and avatar photo."],["UserInfo","The response of a `get_user_information` request."],["UserInfoOptions","The possible options for a get user information request."]]});
--- ashpd/desktop/trash/sidebar-items.js
+++ ashpd/desktop/trash/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["TrashStatus",""]],"struct":[["TrashProxy","The interface lets sandboxed applications send files to the trashcan."]]});
+initSidebarItems({"enum":[["TrashStatus","The status of moving a file to the trash."]],"struct":[["TrashProxy","The interface lets sandboxed applications send files to the trashcan."]]});
