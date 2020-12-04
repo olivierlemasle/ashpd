206139e16d1a789f0fe0bdeb6e04540ef1d3b29a
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2020-12-04 20:25:33 +0000

    Automated deployment: Fri Dec  4 20:25:32 UTC 2020 776db631bfea46452103fb5a2c8bba2bfa35f748

--- implementors/core/convert/trait.Into.js
+++ implementors/core/convert/trait.Into.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["ashpd"] = [{"text":"impl Into&lt;RGBA&gt; for Color","synthetic":false,"types":[]}];
+implementors["ashpd"] = [{"text":"impl Into&lt;RGBA&gt; for Color","synthetic":false,"types":[]},{"text":"impl Into&lt;RGBA&gt; for Color","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
