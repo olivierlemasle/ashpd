849454c088e766f8b01837e608030a28278667b8
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2021-12-28 14:47:12 +0000

    Automated deployment: Tue Dec 28 14:47:12 UTC 2021 dcb23809ec1dd3499457290164ee94979683058d

---
+++ implementors/zbus/dbus_error/trait.DBusError.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["ashpd"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/zbus/latest/zbus/dbus_error/trait.DBusError.html\" title=\"trait zbus::dbus_error::DBusError\">DBusError</a> for <a class=\"enum\" href=\"ashpd/enum.PortalError.html\" title=\"enum ashpd::PortalError\">PortalError</a>","synthetic":false,"types":["ashpd::error::PortalError"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
