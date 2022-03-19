dcf2108a7d5d728b9752c88fa932ad2501907a6d
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2022-03-19 15:42:39 +0000

    Automated deployment: Sat Mar 19 15:42:39 UTC 2022 88779e7b4d1c8634b0a2e745b93bf3f516bf64f5

--- implementors/core/ops/drop/trait.Drop.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["ashpd"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"enum\" href=\"ashpd/enum.WindowIdentifier.html\" title=\"enum ashpd::WindowIdentifier\">WindowIdentifier</a>","synthetic":false,"types":["ashpd::window_identifier::WindowIdentifier"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
