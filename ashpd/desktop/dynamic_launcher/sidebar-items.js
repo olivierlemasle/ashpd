a7979d3773f1f1e6760d44afb102e28b31820782
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2022-05-01 22:12:50 +0000

    Automated deployment: Sun May  1 22:12:50 UTC 2022 d6650b21951f44147072100483d82d04d2a36cfa

---
+++ ashpd/desktop/dynamic_launcher/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["IconType",""],["LauncherType",""]],"struct":[["DynamicLauncherProxy","The interface lets sandboxed applications install launchers like Web Application from your browser or Steam."],["LauncherIcon",""],["PrepareInstallOptions",""]]});
