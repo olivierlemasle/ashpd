6fc91a4338db6f8e2b4cd5a9a3f66afc70de2b39
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2022-12-17 08:43:46 +0000

    Automated deployment: Sat Dec 17 08:43:46 UTC 2022 623fcc81442c63b6ddcd44b81c19916e66b98256

--- implementors/core/convert/trait.TryFrom.js
+++ implementors/core/convert/trait.TryFrom.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"ashpd":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;OwnedValue&gt; for <a class=\"enum\" href=\"ashpd/desktop/enum.Icon.html\" title=\"enum ashpd::desktop::Icon\">Icon</a>"]]
+"ashpd":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;OwnedValue&gt; for <a class=\"enum\" href=\"ashpd/desktop/enum.Icon.html\" title=\"enum ashpd::desktop::Icon\">Icon</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
